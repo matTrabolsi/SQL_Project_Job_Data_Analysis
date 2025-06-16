3d4a432ca4a987aaac431b755a32e57445cc1296
mahmoud trabolsi <mahmoudtarbolsi@gmail.com>
2025-06-16 21:40:34 +0300

    first commit

---
+++ sql_load/datbase.sql
@@ -0,0 +1,42 @@
+-- WITH remote_job_skills AS (
+-- SELECT
+--     skill_id,
+--     COUNT(*) AS skill_count
+-- FROM
+--     skills_job_dim AS skills_to_job
+-- INNER JOIN job_postings_fact AS job_postings ON job_postings.job_id = skills_to_job.job_id
+-- WHERE
+--     job_postings.job_work_from_home = TRUE AND
+--     job_postings.job_title_short = 'Data Analyst'
+-- GROUP BY
+--     skill_id
+-- )
+
+-- SELECT 
+--     skills.skill_id,
+--     skills AS skill_name,
+--     skill_count
+-- FROM remote_job_skills
+-- INNER JOIN skills_dim AS skills ON skills.skill_id = remote_job_skills.skill_id
+-- ORDER BY skill_count DESC 
+
+-- SELECT 
+--     quarter_job_postings.job_location,
+--     quarter_job_postings.job_via,
+--     quarter_job_postings.job_posted_date::DATE,
+--     quarter_job_postings.salary_year_avg
+-- FROM (
+--  SELECT *
+--  FROM january_jobs
+--  UNION ALL
+--  SELECT *
+--  FROM february_jobs
+--  UNION ALL
+--  SELECT *
+--  FROM march_jobs
+-- )AS quarter_job_postings
+-- WHERE 
+--     quarter_job_postings.salary_year_avg > 70000 AND
+--     quarter_job_postings.job_title_short = 'Data Analyst'
+-- ORDER BY
+--     quarter_job_postings.salary_year_avg DESC
